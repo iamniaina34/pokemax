d68a67c2cdf48a2b9de3af599ccbeaa2ce237891
ANDRIANIAINA Mandrindra <iammandrindraniaina34@gmail.com>
2024-07-19 21:48:27 +0300

    Implemented: Themes

---
+++ src/utilities/themes.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material";
+
+export const defaultTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#ce1126',
+        },
+        secondary: {
+            main: '#f7cb15',
+        },
+        background: {
+            default: '#f5f5f5',
+        },
+        text: {
+            primary: '#000000',
+        },
+    }
+})
